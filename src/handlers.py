bc811d7c4d5ad1e5991455d28d360d73c15c3874
mrdragar <yshhenyaev@mail.ru>
2025-02-14 01:18:31 +0300

    add censorship

--- src/handlers.py
+++ src/handlers.py
@@ -12,6 +12,14 @@ async def post_clown(bot: Client, message: Message) -> None:
     logging.info(f"got message from {message.from_user.id}")
     if message.from_user.id != int(CLOWN_ID):
         return
+
+    if all((symbol in "zv" or not symbol.isalpha() for symbol in message.text.strip().lower())):
+        return logging.info("Is ZV") and None
+    if "росси" in message.text.strip().lower():
+        return logging.info("About Russian") and None
+    if "путин" in message.text.strip().lower():
+        return logging.info("About Putin") and None
+
     logging.info(f"{message.from_user.id} is a clown")
     await message.react(emoji=CLOWN_FACE)
 
